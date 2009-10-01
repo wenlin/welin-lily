72fad9b4c85c76345d3cb3adf104b910cf9a0c40
Yan Long <yanlong@maimang.org>
2009-10-01 14:58:22 +0800

    Stuff logical improvement

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,3 +1,5 @@
 class Stuff < ActiveRecord::Base
 validates_presence_of :subject_id, :day_id, :sn, :name, :status
+validates_length_of :name, :maximum => 10
+validates_uniqueness_of :name, :on => :create, :message => "不能与现有项重复"
 end
