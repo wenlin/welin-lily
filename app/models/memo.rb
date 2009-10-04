cff9aa68c222f9c082b5666262ad23e461bbec7a
Yan Long <yanlong@maimang.org>
2009-10-04 20:52:56 +0800

    Some fix

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,9 +2,6 @@ class Memo < ActiveRecord::Base
 validates_presence_of :name, :p_next, :t_next,:m, :n
 validates_numericality_of :p_next, :t_next,:m, :n
 
-validates_presence_of :t_first, :on => :update
-validates_numericality_of :t_first, :on => :update
-
 validates_length_of :name, :maximum => 10
 validates_uniqueness_of :name, :on => :create, :message => "不能与现有项重复"
 
