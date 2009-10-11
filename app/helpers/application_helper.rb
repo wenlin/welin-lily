104bcca6a2e8868d008d6439f5c2ce47751326b1
Yan Long <yanlong@maimang.org>
2009-10-11 12:18:38 +0800

    Fixment for the morning p err. but will it works?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,13 @@ module ApplicationHelper
 	offset = Rational(8, 24)
 	t = utc.new_offset(offset)
 	njcee = DateTime.new(2010, 6, 7)
-	return @days_to_njcee = ( njcee - t).to_i
+	@days_to_njcee = ( njcee - t).to_i
+	if t.zone != "+0800" or t.zone != "+08:00"
+	  if t.hour < 8
+		@days_to_njcee = @days_to_njcee + 1
+	  end
+	end
+	return @days_to_njcee
   end
 
 end
